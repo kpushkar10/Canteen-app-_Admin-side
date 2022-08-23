87f6000a2f2141e3322eafad5ff90ea10996810b
NandniJogiji <81563404+NandniJogiji@users.noreply.github.com>
2022-08-23 15:06:57 +0530

    Admin_Nav_Bar

---
+++ nav_bar.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:line_icons/line_icons.dart';
+
+class NavBar extends StatefulWidget {
+  @override
+  _NavBarState createState() => _NavBarState();
+}
+
+class _NavBarState extends State<NavBar> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.w600);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Order',
+      style: optionStyle,
+    ),
+    Text(
+      'Menu',
+      style: optionStyle,
+    ),
+    Text(
+      'Dashboard',
+      style: optionStyle,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 20,
+        title: const Text('GoogleNavBar'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 20,
+              color: Colors.black.withOpacity(.1),
+            )
+          ],
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+            child: GNav(
+              rippleColor: Colors.grey[300]!,
+              //rippleColor: Colors.purple,
+              hoverColor: Colors.grey[100]!,
+              //hoverColor: Colors.purple,
+              gap: 8,
+              activeColor: Colors.white,
+              iconSize: 24,
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+              duration: Duration(milliseconds: 400),
+              //tabBackgroundColor: Colors.grey[100]!,
+              tabBackgroundColor: Colors.orange.shade900,
+              color: Colors.black,
+              tabs: const [
+                GButton(
+                  icon: LineIcons.home,
+                  text: 'Order',
+                ),
+                GButton(
+                  icon: LineIcons.heart,
+                  text: 'Menu',
+                ),
+                GButton(
+                  icon: LineIcons.search,
+                  text: 'Dashboard',
+                ),
+              ],
+              selectedIndex: _selectedIndex,
+              onTabChange: (index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
